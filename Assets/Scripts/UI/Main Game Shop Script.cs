bb42584bfbb8bfa9cc4a3084b3eb7d75a8a64e7f
G.Dávid <davidmaestro444@gmail.com>
2025-10-26 09:54:08 +0100

    Robot megadott helyre való lerakása
    A robot csak a kijelölt helyekre rakható le a bal klikkel. Jobb klikkel meg lehet szakítani a lerakást.

--- Assets/Scripts/UI/Main Game Shop Script.cs
+++ Assets/Scripts/UI/Main Game Shop Script.cs
@@ -5,43 +5,65 @@ using UnityEngine.UI;
 
 public class ButtonScript : MonoBehaviour
 {
-
     public GameObject robot;
-    public Button button;
     public Camera mainCamera;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        button.onClick.AddListener(SpawnRobot);
-    }
+    private GameObject currentRobot;
 
-    // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    void SpawnRobot()
-    {
-        Vector2 pos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-
-        GameObject newRobot = Instantiate(robot, pos, Quaternion.identity);
-
-        StartCoroutine(MoveRobotAfterBuying(newRobot));
-
-        
-        Debug.Log("Button pressed");
-    }
-
-    IEnumerator MoveRobotAfterBuying(GameObject robot)
-    {
-        while (!Mouse.current.leftButton.isPressed)
+        if (currentRobot == null)
         {
-            Vector2 pos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-            robot.transform.position = pos;
+            return;
+        }
 
-            yield return null;
+        Vector2 pos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        currentRobot.transform.position = pos;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            PlaceRobot();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            CancelPlacement();
         }
     }
+
+    public void SelectRobotToSpawn()
+    {
+        if (currentRobot != null)
+        {
+            return;
+        }
+
+        currentRobot = Instantiate(robot, Vector3.zero, Quaternion.identity);
+    }
+
+    private void PlaceRobot()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(mainCamera.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+        if (hit.collider != null && hit.collider.CompareTag("TowerSpot"))
+        {
+            currentRobot.transform.position = hit.collider.transform.position;
+            Debug.Log("Robot lerakva ide: " + hit.collider.name);
+
+            currentRobot = null;
+        }
+        else
+        {
+            Debug.Log("Ide nem lehet lerakni!");
+        }
+    }
+
+    private void CancelPlacement()
+    {
+        Debug.Log("Lerakás megszakítva.");
+        Destroy(currentRobot);
+        currentRobot = null;
+    }
+
+
 }
